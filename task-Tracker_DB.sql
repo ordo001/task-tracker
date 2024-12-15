b37edfeafed350753fb4669fd9ccf1f989361c4a
ordo001 <113034616+ordo001@users.noreply.github.com>
2024-12-16 01:52:43 +0300

    3.0.1

--- task-Tracker_DB.sql
+++ task-Tracker_DB.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Дек 12 2024 г., 21:39
+-- Время создания: Дек 16 2024 г., 01:52
 -- Версия сервера: 8.0.30
 -- Версия PHP: 8.1.9
 
@@ -37,6 +37,22 @@ CREATE TABLE `cards` (
   `cardLocation_Y` int NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Дамп данных таблицы `cards`
+--
+
+INSERT INTO `cards` (`id_card`, `name_card`, `heading`, `content`, `id_user`, `id_section`, `cardLocation_Y`) VALUES
+(34, 'card0', 'Heading', '', 5, 52, 55),
+(35, 'card0', 'Heading', 'dfsdfsdfdsf', 5, 53, 55),
+(36, 'card0', 'Heading', '', 5, 55, 55),
+(37, 'card0', 'Heading', 'dfgfdgfdgdfgfdgfd', 5, 54, 55),
+(38, 'card0', 'Heading', '', 5, 60, 55),
+(39, 'card36', 'Heading', '', 5, 55, 275),
+(40, 'card39', 'Heading', 'zxczxczxczxcz', 5, 55, 495),
+(41, 'card40', 'Heading', '', 5, 55, 715),
+(42, 'card0', 'Heading', 'dfsvsvsxsd', 5, 61, 55),
+(43, 'card42', 'Heading', '', 5, 61, 275);
+
 -- --------------------------------------------------------
 
 --
@@ -57,11 +73,13 @@ CREATE TABLE `sections` (
 --
 
 INSERT INTO `sections` (`id_section`, `id_work_space`, `name_section`, `heading_section`, `sectionLocation_X`, `sectionLocation_Y`) VALUES
-(6, 1, 'section0', 'Новая секция', 10, 140),
-(7, 2, 'section0', 'Новая секция', 10, 140),
-(8, 3, 'section0', 'Новая секция', 10, 140),
-(9, 3, 'section8', 'Новая секция', 470, 140),
-(10, 2, 'section7', 'Новая секция', 470, 140);
+(52, 12, 'section0', 'Новая секция', 10, 140),
+(53, 13, 'section0', 'Новая секция', 10, 140),
+(54, 14, 'section0', 'Новая секция', 10, 140),
+(55, 13, 'section53', 'Новая секция', 470, 140),
+(56, 13, 'section55', 'Новая секция', 930, 140),
+(60, 14, 'section54', 'Новая секция', 470, 140),
+(61, 12, 'section52', 'Новая секция', 470, 140);
 
 -- --------------------------------------------------------
 
@@ -82,10 +100,16 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id_user`, `login`, `password`, `role`, `name`) VALUES
-(1, 'test', 'test', 'Default', 'testName'),
+(1, 'test', 'test', 'Moderator', 'testName'),
 (3, 'aboba', '123', 'Default', 'abobaName'),
 (4, 'test2', 'test2', 'Default', 'testName'),
-(5, 'admin', 'admin', 'Administrator', 'DungeonMaster');
+(5, 'admin', 'admin', 'Administrator', 'DungeonMaster'),
+(6, 'aboba2', 'aboba2', 'Default', 'aboba2'),
+(7, 'aboba3', 'aboba3', 'Default', 'aboba3'),
+(8, 'aboba4', 'aboba4', 'Default', 'aboba4'),
+(9, 'aboba5', 'aboba5', 'Default', 'aboba5'),
+(10, 'aboba6', 'aboba6', 'Default', 'aboba6'),
+(11, 'aboba7', 'aboba7', 'Default', 'aboba7');
 
 -- --------------------------------------------------------
 
@@ -103,10 +127,17 @@ CREATE TABLE `user_work_space` (
 --
 
 INSERT INTO `user_work_space` (`user_id`, `work_space_id`) VALUES
-(1, 1),
-(3, 1),
-(4, 1),
-(1, 2);
+(1, 12),
+(3, 12),
+(5, 12),
+(9, 12),
+(1, 13),
+(3, 13),
+(4, 13),
+(6, 13),
+(7, 13),
+(9, 14),
+(11, 14);
 
 -- --------------------------------------------------------
 
@@ -116,17 +147,18 @@ INSERT INTO `user_work_space` (`user_id`, `work_space_id`) VALUES
 
 CREATE TABLE `work_spaces` (
   `id_work_space` int NOT NULL,
-  `work_space_name` varchar(100) NOT NULL
+  `work_space_name` varchar(100) NOT NULL,
+  `image` varbinary(65000) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Дамп данных таблицы `work_spaces`
 --
 
-INSERT INTO `work_spaces` (`id_work_space`, `work_space_name`) VALUES
-(1, 'Информационные технологии'),
-(2, 'Тестовое пространство 1'),
-(3, 'Тестовое пространство 2');
+INSERT INTO `work_spaces` (`id_work_space`, `work_space_name`, `image`) VALUES
+(12, 'Типа доска 1', NULL),
+(13, 'Типа доска 2', NULL),
+(14, 'Типа доска 3', NULL);
 
 --
 -- Индексы сохранённых таблиц
@@ -174,29 +206,35 @@ ALTER TABLE `work_spaces`
 -- AUTO_INCREMENT для таблицы `cards`
 --
 ALTER TABLE `cards`
-  MODIFY `id_card` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_card` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT для таблицы `sections`
 --
 ALTER TABLE `sections`
-  MODIFY `id_section` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_section` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=62;
 
 --
 -- AUTO_INCREMENT для таблицы `users`
 --
 ALTER TABLE `users`
-  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT для таблицы `work_spaces`
 --
 ALTER TABLE `work_spaces`
-  MODIFY `id_work_space` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id_work_space` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- Ограничения внешнего ключа сохраненных таблиц
 --
+
+--
+-- Ограничения внешнего ключа таблицы `cards`
+--
+ALTER TABLE `cards`
+  ADD CONSTRAINT `cards_ibfk_1` FOREIGN KEY (`id_section`) REFERENCES `sections` (`id_section`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
 --
 -- Ограничения внешнего ключа таблицы `sections`
